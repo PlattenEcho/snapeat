33474f17d04ad6b5f3b34e8649ceac979164dd67
PlattenEcho <akunsekolahdika@gmail.com>
2024-06-02 15:54:17 +0700

    Implement UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,16 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.runtime.android)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("androidx.activity:activity-compose:1.9.0")
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-graphics")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material3:material3")
 
     // Splash API
     implementation ("androidx.core:core-splashscreen:1.0.1")
